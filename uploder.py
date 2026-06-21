078da0bd384f3f74a7e967f36ada898c9bd62ee6
pryme <flatcon11@gmail.com>
2026-06-21 13:05:59 +0200

    new

--- uploder.py
+++ uploder.py
@@ -2,6 +2,14 @@ import os
 
 commit_message = input("Commit message: ")
 
-os.system("git add .")
+# 1. ensure you're on main branch
+os.system("git checkout main")
+
+# 2. stage everything (new + deleted files too)
+os.system("git add -A")
+
+# 3. commit
 os.system(f'git commit -m "{commit_message}"')
-os.system("git push")
+
+# 4. push safely
+os.system("git push origin main")
